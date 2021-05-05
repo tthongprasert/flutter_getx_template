528ac26820b5bd56f5b632873e796bf9d4729b20
Thitiworada Thongprasert <tt@manaosoftware.com>
2021-05-05 23:28:40 +0700

    Remove unused files

--- lib/app/binding/login_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_getx_template/app/controllers/login_controller.dart';
-import 'package:get/get.dart';
-
-class LoginBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<LoginController>(() => LoginController());
-  }
-}
